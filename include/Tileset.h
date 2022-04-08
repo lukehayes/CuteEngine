794e16b4e4e622017dbd78796c30df34267d87b0
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-08 10:22:30 +0100

    Added Tileset::DrawTile() method.

--- include/Tileset.h
+++ include/Tileset.h
@@ -29,6 +29,23 @@ public:
         return this->texture.Draw(x,y);
     }
 
+    /**
+     * Draw a section of the texture.
+     *
+     * @param float x    The x position of the texture.
+     * @param float y    The y position of the texture.
+     *
+     * @return raylib::Texture&
+     */
+    raylib::Texture& DrawTile(float x = 0, float y = 0, float xp = 0, float yp = 0)
+    {
+        float space = 1;
+        float scale = 5.0f;
+        raylib::Rectangle src = {space + 16 * xp, space + 16 * yp, 32, 32};
+        raylib::Rectangle dest = {x,y, 16 * scale, 16 * scale};
+        return this->texture.DrawTiled(src, dest, {0,0}, 0, scale);
+    }
+
 private:
     raylib::Texture texture;
 };
