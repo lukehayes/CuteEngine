2edd82b56996691357f353b0cf70c18e54886635
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-08 10:22:41 +0100

    Updated main with latest classes.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "raylib-cpp.hpp"
 #include "Tileset.h"
+#include "Sprite.h"
 
 int main() {
     int screenWidth = 800;
@@ -13,23 +14,27 @@ int main() {
 
     static float c = 0.0;
 
-    Tileset ts("assets/bernie.png");
+    Tileset ts("assets/monochrome_tilemap_transparent.png");
+    Sprite sp("assets/bernie.png");
 
 
     while (!window.ShouldClose())
     {
-        c += 0.01;
+        c += 0.05;
 
         BeginDrawing();
 
-        ts.Draw(200,200);
+        //ts.Draw(200,200);
+
+        //sp.Draw(100,100);
+        ts.DrawTile(100,100,c,1);
         window.ClearBackground(BLACK);
 
         // Object methods.
-        logo.Draw(
-            (std::cos(c) * 100)  + screenWidth / 2 - logo.GetWidth() / 2,
-            (std::sin(c) * 100) + screenHeight / 2 - logo.GetHeight() / 2);
-        DrawText("Bernie says, build that f*****g game!", 190, 100, 20, LIGHTGRAY);
+        //logo.Draw(
+            //(std::cos(c) * 100)  + screenWidth / 2 - logo.GetWidth() / 2,
+            //(std::sin(c) * 100) + screenHeight / 2 - logo.GetHeight() / 2);
+        //DrawText("Bernie says, build that f*****g game!", 190, 100, 20, LIGHTGRAY);
         EndDrawing();
 
     }
