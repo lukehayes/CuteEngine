b8325b00baf4baec1b9072c6262387e44fa8e561
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-14 17:49:29 +0100

    Updated main with latest test code.

--- main.cpp
+++ main.cpp
@@ -2,31 +2,15 @@
 #include "raylib-cpp.hpp"
 #include "Tileset.h"
 #include "Sprite.h"
+#include "Entity.h"
 #include "Collision.h"
 #include "CollisionRect.h"
 #include <iostream>
 
 float delta = 0.0;
 float speed = 10.0;
-float size = 10.0;
-float MAX = 1000.0;
-
-class Entity
-{
-public:
-    Entity(float x, float y) : cr(x,y,16,16), sprite("assets/player.png", 3.0){}
-    void Draw() { sprite.Draw(x, y); }
-    void Update() {
-        cr.rect.x = this->x;
-        cr.rect.y = this->y;
-        std::cout << x << std::endl;
-    }
-    CollisionRect cr;
-    Sprite sprite;
-    Color color = PINK;
-    float x;
-    float y;
-};
+float size = 30.0;
+float MAX = 10.0;
 
 int main() {
     int screenWidth = 800;
@@ -39,24 +23,26 @@ int main() {
     static float c = 0.0;
 
     Tileset ts("assets/8x8.png", 8, 4, 7,11);
-    Sprite sp("assets/bernie.png", 1, 180);
+    Sprite sp("assets/bernie.png", 300,300, 1, 180);
 
     float xp = 10.0f;
     float yp = 10.0f;
 
     Rectangle a = {(float)screenWidth / 2, (float)screenHeight / 2, size, size};
     Rectangle b = {150,120, size, size};
+    Entity e(20,20, "assets/player.png");
     CollisionRect cr(100,200,100,100);
-    Entity e(screenWidth/2, screenHeight/2);
 
     Vector2 vel = {0,0};
 
-    std::vector<Rectangle> rects;
+    std::vector<CollisionRect> rects;
 
     for(int i = 0; i <= MAX - 1; i++)
     {
-        Rectangle r = {(float)GetRandomValue(0, 750), (float)GetRandomValue(0, 600), size, size};
+        CollisionRect r = {(float)GetRandomValue(0, 750), (float)GetRandomValue(0, 600), size, size};
+        CollisionRect fr = {(float)i * 10, 580, size, size};
         rects.push_back(r);
+        rects.push_back(fr);
     }
 
     while (!window.ShouldClose())
@@ -96,38 +82,47 @@ int main() {
             vel.y = -(vel.y / 2.0);
         }
 
-        if(cr.CollidesWith(e.cr))
-        {
-            vel.x = -(vel.x / 2);
-            vel.y = -(vel.y / 2);
-        }
-
-
         for(auto r : rects)
         {
-            if(Collision::AABB(a,r))
+            if(e.colRect.CollidesWith(r))
             {
-                vel.x = -vel.x;
-                vel.y = -vel.y;
+                vel.x = -(vel.x / 2);
+                vel.y = -(vel.y / 2);
+
             }
         }
+        //if(cr.CollidesWith(e.colRect))
+        //{
+            //vel.x = -(vel.x / 2);
+            //vel.y = -(vel.y / 2);
 
+        //}
+
+        //for(auto r : rects)
+        //{
+            //if(Collision::AABB(a,r))
+            //{
+                //vel.x = -vel.x;
+                //vel.y = -vel.y;
+            //}
+        //}
 
         BeginDrawing();
-            window.ClearBackground(BLACK);
+            window.ClearBackground({41, 30, 49});
 
-            //DrawRectangle(a.x, a.y, a.width, a.height, GREEN);
+            DrawRectangle(a.x, a.y, a.width, a.height, GREEN);
             DrawRectangle(b.x, b.y, b.width, b.height, RED);
-            DrawRectangle(cr.rect.x, cr.rect.y, cr.rect.width, cr.rect.height, ORANGE);
 
-            //DrawRectangleLines(e.cr.rect.x, e.cr.rect.y, e.cr.rect.width, e.cr.rect.height, PURPLE);
+            DrawRectangle(cr.rect.x, cr.rect.y, cr.rect.width, cr.rect.height, cr.color);
+
             e.Draw();
+            DrawRectangleLines(e.colRect.rect.x, e.colRect.rect.y, e.colRect.rect.width, e.colRect.rect.height, e.colRect.color);
 
 
-            //for(auto r : rects)
-            //{
-                //DrawRectangle(r.x, r.y, r.width, r.height, BLUE);
-            //}
+            for(auto r : rects)
+            {
+                DrawRectangle(r.rect.x, r.rect.y, r.rect.width, r.rect.height, BLUE);
+            }
 
         EndDrawing();
 
