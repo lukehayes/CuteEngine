2141d27a53f0675350474c80c8bed7f25834f4fe
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-28 09:06:36 +0100

    Added Object class to project.

---
+++ include/Object.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include "BasicObject.h"
+
+class Object : public BasicObject
+{
+    public:
+        Object();
+        ~Object();
+};
+
+#endif
---
+++ src/Object.cpp
@@ -0,0 +1,5 @@
+#include "Object.h"
+#include <iostream>
+
+Object::Object() { std::cout << "Object Created" << std::endl;}
+Object::~Object() { std::cout << "Object Destroyed" << std::endl;}
