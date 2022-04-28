550d9d244cd6ad7d67c06f9507ee17768ae5c9bd
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-28 09:11:33 +0100

    Added Vector2 position member to Object class.

--- include/Object.h
+++ include/Object.h
@@ -2,12 +2,15 @@
 #define OBJECT_H
 
 #include "BasicObject.h"
+#include "raylib.h"
 
 class Object : public BasicObject
 {
     public:
         Object();
         ~Object();
+
+        Vector2 position;
 };
 
 #endif
--- src/Object.cpp
+++ src/Object.cpp
@@ -1,5 +1,7 @@
 #include "Object.h"
 #include <iostream>
 
-Object::Object() { std::cout << "Object Created" << std::endl;}
-Object::~Object() { std::cout << "Object Destroyed" << std::endl;}
+Object::Object() 
+    : position(0,0)
+{}
+Object::~Object() {}
