664f4c097f906c5ab4f4eec8b62f839ce13dc9ad
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-28 08:58:41 +0100

    Added BasicObject class to the project.

---
+++ include/BasicObject.h
@@ -0,0 +1,15 @@
+#ifndef BASIC_OBJECT_H
+#define BASIC_OBJECT_H
+
+/**
+ * Most if not all classes should inherti from this class.
+ */
+class BasicObject
+{
+    public:
+        BasicObject();
+        ~BasicObject();
+};
+
+
+#endif
---
+++ src/BasicObject.cpp
@@ -0,0 +1,5 @@
+#include "BasicObject.h"
+#include <iostream>
+
+BasicObject::BasicObject() { std::cout << "BasicObject Created" << std::endl;}
+BasicObject::~BasicObject() { std::cout << "BasicObject Destroyed" << std::endl;}
