752ca477edd2ea4df22d2f6a3fc2eb67412a55be
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-17 17:36:41 +0100

    Added Entity test to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "raylib.h"
 #include "game.h"
+#include "entity/entity.h"
+#include <vector>
 
 float delta = 0.0;
 
@@ -7,12 +9,31 @@ int main() {
 
 	game.init();
 
+	std::vector<Entity> entities;
+
+	for(int i = 0; i <= 100; i++)
+	{
+		Entity e;
+		entities.push_back(e);
+	}
+
     while (!WindowShouldClose())
     {
         delta = GetFrameTime();
 
+		for(auto &e : entities)
+		{
+			e.update(delta);
+		}
+
 		BeginDrawing();
 			ClearBackground(GRAY);
+
+			for(auto e : entities)
+			{
+				e.render();
+			}
+
         EndDrawing();
     }
 
