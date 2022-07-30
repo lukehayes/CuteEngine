9228ddff82a23ab45bcc31aa38fef7b4f5493d2a
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-30 09:21:34 +0100

    Added ds/quad-tree.h to project.

---
+++ include/ds/quad-tree.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+
+/**
+ * A very basic implementation of a quad tree.
+ */
+class QuadTree
+{
+	public:
+		QuadTree() {}
+		~QuadTree() {}
+
+};
