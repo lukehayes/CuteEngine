d6926b1e0b2e4b50c39db380f002df773f83069c
Luke Hayes <lukepurplehayes@gmail.com>
2022-08-23 15:08:15 +0100

    Bullet now accepts helpful arguments into its constructor.

--- include/bullet/bullet.h
+++ include/bullet/bullet.h
@@ -4,20 +4,20 @@
 
 class Bullet : public Entity
 {
-public:
-	Bullet(const Vector2& position, const Vector2& size, const float speed);
-  virtual ~Bullet();
+    public:
+        Bullet(const Vector2& position, const Vector2& target);
+        virtual ~Bullet();
 
-	virtual void render();
-	virtual void update(double dt);
+        virtual void render();
+        virtual void update(double dt);
 
 
-	Color   color;
-	Vector2 target;
-	float   angle;
-	int     damage;
+        Color   color;
+        Vector2 target;
+        float   angle;
+        int     damage;
 
-private:
-	float getTargetAngle() const;
+    private:
+        float getTargetAngle() const;
 };
 
--- src/bullet.cpp
+++ src/bullet.cpp
@@ -1,33 +1,33 @@
 #include "bullet/bullet.h"
 #include <cmath>
 
-Bullet::Bullet(const Vector2& position, const Vector2& size, const float speed)
-	: Entity(position, {5,5}, 80),
-		color({150,50,200, 255}),
+Bullet::Bullet(const Vector2& position, const Vector2& target)
+    : Entity(position, {5,5}, 200),
+    color({150,50,200, 255}),
     target(target),
     angle(this->getTargetAngle()),
     damage(1) {}
 
-Bullet::~Bullet() {}
+    Bullet::~Bullet() {}
 
 void Bullet::render()
 {
-	DrawRectangle(
-			position.x,
-			position.y,
-			size.x,
-			size.y,
-			color
-			);
+    DrawRectangle(
+            position.x,
+            position.y,
+            size.x,
+            size.y,
+            color
+            );
 
 }
 void Bullet::update(double dt)
 {
-  position.x -= std::cos(angle) * speed * dt;
-  position.y -= std::sin(angle) * speed * dt;
+    position.x -= std::cos(angle) * speed * dt;
+    position.y -= std::sin(angle) * speed * dt;
 }
 
 float Bullet::getTargetAngle() const
 {
-  return std::atan2(position.y - target.y, position.x - target.x);
+    return std::atan2(position.y - target.y, position.x - target.x);
 }
