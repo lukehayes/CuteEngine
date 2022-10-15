54de3ff1e6bdc925e60732ce6e412767542f5ef5
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-15 09:37:08 +0100

    Cleaned up main.cpp

--- main.cpp
+++ main.cpp
@@ -7,28 +7,12 @@ int main() {
 
     game.init();
 
-    std::shared_ptr<Entity> p = std::make_shared<Player>(Vector2{((float)game.width - 25)/ 2,((float)game.height - 25) / 2});
-
-    TypedContainer<Bullet> bullets;
-
     while (!WindowShouldClose())
     {
         delta = GetFrameTime();
 
-
-        if(IsMouseButtonDown(0))
-        {
-            std::shared_ptr<Bullet> b = std::make_shared<Bullet>(p->position, GetMousePosition());
-            bullets.add(b);
-        }
-
-        p->update(delta);
-        bullets.update(delta);
-
         ClearBackground(GRAY);
         BeginDrawing();
-            p->render();
-            bullets.render();
         EndDrawing();
     }
 
