48e4867bd86b2a36d81bf8fcb27fd528c21bc32e
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-19 10:57:32 +0100

    Updated main with latest test code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include "common/types.h"
 #include "ECS/World.h"
 #include "ECS/System/System.h"
+#include "ECS/Entity/Entity.h"
+#include "ECS/Component/PositionComponent.h"
+#include "ECS/Component/Component.h"
+#include <memory>
+
 
 float delta = 0.0;
+
+using ComponentPtr = std::shared_ptr<ECS::Component::Component>;
 
 int main() {
 
@@ -10,6 +17,17 @@ int main() {
 
     ECS::World world;
     ECS::System::System system;
+    
+    ECS::Component::PositionComponent pos(100,100);
+
+    ECS::Entity::Entity p;
+    ComponentPtr pos = std::make_shared<ECS::Component::Component>(11.0,23.0)>
+
+    p.addComponent("Position", pos);
+
+    //std::pair<std::string, ECS::Component::Component> comp = p.getComponent("Position");
+
+    //std::cout << comp.second->x << std::endl;
 
     while (!WindowShouldClose())
     {
