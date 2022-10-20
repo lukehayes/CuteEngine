694528d34f58c250be1443d8f83f35bccfa106df
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-20 10:04:14 +0100

    Added color component to multiple entities.

--- main.cpp
+++ main.cpp
@@ -22,12 +22,17 @@ int main() {
 
     std::vector<ECS::Entity::Entity> entities;
 
-    for(int i = 0; i <= 10; i++)
+    for(int i = 0; i <= 1000; i++)
     {
-
         ECS::Entity::Entity entity;
         std::shared_ptr<ECS::Component::Component> pos = std::make_shared<ECS::Component::PositionComponent>(GetRandomValue(10,400), GetRandomValue(10,400));
+        std::shared_ptr<ECS::Component::Component> col = std::make_shared<ECS::Component::ColorComponent>(
+                GetRandomValue(0.0,1.0),
+                GetRandomValue(0.0,1.0),
+                GetRandomValue(0.0,1.0)
+        );
         entity.addComponent("Position", pos);
+        entity.addComponent("Color", col);
         entities.push_back(entity);
     }
 
@@ -53,7 +58,8 @@ int main() {
             for(auto &e : entities)
             {
                 auto posComp = std::dynamic_pointer_cast<ECS::Component::PositionComponent>(e.getComponent("Position"));
-                DrawRectangle(posComp->x, posComp->y, 10,10, BLACK);
+                auto colComp = std::dynamic_pointer_cast<ECS::Component::ColorComponent>(e.getComponent("Color"));
+                DrawRectangle(posComp->x, posComp->y, 10,10, (Color){1,0,0});
             }
 
         EndDrawing();
