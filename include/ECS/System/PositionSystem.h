8685981d036a74ce4200bbaef25b03e60efbdeed
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-22 10:29:14 +0100

    Added simple edge of screen collision check

--- include/ECS/System/PositionSystem.h
+++ include/ECS/System/PositionSystem.h
@@ -15,8 +15,18 @@ namespace ECS::System
             {
                 for(auto& component : this->components)
                 {
-                    component->x += component->vx * delta;
-                    component->y += component->vy * delta;
+                    component->x += component->speed * component->vx * delta;
+                    component->y += component->speed * component->vy * delta;
+
+                    if(component->x < 0 || component->x >= game.width)
+                    {
+                        component->vx = -component->vx;
+                    }
+
+                    if(component->y < 0 || component->y >= game.width)
+                    {
+                        component->vy = -component->vy;
+                    }
 
                     DrawRectangle(
                             component->x,
