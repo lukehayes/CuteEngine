bc38ab0702b801c101c8ce44c26deb7f168aa1fd
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-25 14:44:11 +0100

    Added simple working polymorphic test to main.

--- main.cpp
+++ main.cpp
@@ -6,30 +6,49 @@
 #include <memory>
 
 float delta = 0.0;
-
-using ComponentPtr = std::shared_ptr<ECS::Component::Component>;
-using PositionPtr = std::shared_ptr<ECS::Component::PositionComponent>;
-using PositionComp = ECS::Component::PositionComponent;
-using Entity = ECS::Entity::Entity;
+//
+// class A
+// {
+//   public:
+//     A(int x) : x(x) {}
+//     int x;
+// };
+//
+// class B : public A
+// {
+//   public:
+//     B(int x) : A(x) {}
+//     int x;
+// };
 
 int main() {
 
   game.init();
+
+  // std::shared_ptr<A> base = std::make_shared<A>(1);
+  // std::shared_ptr<A> child = std::make_shared<B>(2);
+  // std::cout << base->x << std::endl;
+  // std::cout << child->x << std::endl;
+
 
   ECS::World world;
   ECS::System::PositionSystem positionSystem;
 
   std::vector<ECS::Entity::Entity> entities;
 
-  // FIXME Workout and refactor this.
+  // FIXME Work out and refactor this.
   //
   /* == Temporary Working Solution ==================================== */
   ECS::Entity::Entity e(1);
   std::shared_ptr<ECS::Component::Component> posPtr = std::make_shared<ECS::Component::PositionComponent>(100,100,10,10);
   e.addComponent("Position", posPtr);
+
+  // std::shared_ptr<ECS::Component::PositionComponent> pos = e.getComponent("Position");
   std::shared_ptr<ECS::Component::PositionComponent> pos = std::dynamic_pointer_cast<ECS::Component::PositionComponent>(e.getComponent("Position"));
   /* ================================================================== */
 
+  std::cout << pos->x << std::endl;
+  
   float speed = 10.0;
 
   while (!WindowShouldClose()) {
