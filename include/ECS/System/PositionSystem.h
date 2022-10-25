d760ffef0aa9a092f39f399c56c4eec2b2508ec7
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-25 17:30:06 +0100

    Updated PositionSystem.h

--- include/ECS/System/PositionSystem.h
+++ include/ECS/System/PositionSystem.h
@@ -2,7 +2,10 @@
 
 #include "ECS/System/System.h"
 #include "ECS/Component/PositionComponent.h"
+#include "ECS/World.h"
 #include <memory>
+
+extern ECS::World world;
 
 namespace ECS::System
 {
@@ -14,22 +17,28 @@ namespace ECS::System
 
             void update(float delta)
             {
-                for(auto& component : this->components)
-                {
-                    component->x += component->speed * component->vx * delta;
-                    component->y += component->speed * component->vy * delta;
+                std::vector<ECS::Entity::Entity> ents;
+                int requirement_size = this->requirements.size();
 
-                    if(component->x < 0 || component->x >= game.width)
-                    {
-                        component->vx = -component->vx;
-                    }
+                // TODO Implement entity selection based on requirements.
 
-                    if(component->y < 0 || component->y >= game.width)
-                    {
-                        component->vy = -component->vy;
-                    }
 
-                }
+                // for(auto& component : this->components)
+                // {
+                //     component->x += component->speed * component->vx * delta;
+                //     component->y += component->speed * component->vy * delta;
+                //
+                //     if(component->x < 0 || component->x >= game.width)
+                //     {
+                //         component->vx = -component->vx;
+                //     }
+                //
+                //     if(component->y < 0 || component->y >= game.width)
+                //     {
+                //         component->vy = -component->vy;
+                //     }
+                //
+                // }
             }
 
         std::vector< std::shared_ptr<ECS::Component::PositionComponent> > components;
--- main.cpp
+++ main.cpp
@@ -2,60 +2,39 @@
 #include "ECS/Component/Component.h"
 #include "ECS/Component/PositionComponent.h"
 #include "ECS/System/PositionSystem.h"
+#include "ECS/System/System.h"
 #include "ECS/World.h"
 #include <memory>
 
 float delta = 0.0;
-//
-// class A
-// {
-//   public:
-//     A(int x) : x(x) {}
-//     int x;
-// };
-//
-// class B : public A
-// {
-//   public:
-//     B(int x) : A(x) {}
-//     int x;
-// };
 
 int main() {
 
   game.init();
 
-  // std::shared_ptr<A> base = std::make_shared<A>(1);
-  // std::shared_ptr<A> child = std::make_shared<B>(2);
-  // std::cout << base->x << std::endl;
-  // std::cout << child->x << std::endl;
-
-
-  ECS::World world;
   ECS::System::PositionSystem positionSystem;
-
-  std::vector<ECS::Entity::Entity> entities;
+  positionSystem.required({"Position", "Sprite"});
 
   // FIXME Work out and refactor this.
   //
   /* == Temporary Working Solution ==================================== */
   ECS::Entity::Entity e(1);
+  ECS::Entity::Entity ee(2);
   std::shared_ptr<ECS::Component::Component> posPtr = std::make_shared<ECS::Component::PositionComponent>(100,100,10,10);
   e.addComponent("Position", posPtr);
 
-  // std::shared_ptr<ECS::Component::PositionComponent> pos = e.getComponent("Position");
-  std::shared_ptr<ECS::Component::PositionComponent> pos = std::dynamic_pointer_cast<ECS::Component::PositionComponent>(e.getComponent("Position"));
-  /* ================================================================== */
+  world.entities.push_back(e);
 
-  std::cout << pos->x << std::endl;
-  
-  float speed = 10.0;
+  // std::shared_ptr<ECS::Component::PositionComponent> pos = e.getComponent("Position");
+  // std::shared_ptr<ECS::Component::PositionComponent> pos = std::dynamic_pointer_cast<ECS::Component::PositionComponent>(e.getComponent("Position"));
+  /* ================================================================== */
 
   while (!WindowShouldClose()) {
     delta = GetFrameTime();
     world.update(delta);
 
     positionSystem.update(delta);
+
 
     ClearBackground(GRAY);
     BeginDrawing();
