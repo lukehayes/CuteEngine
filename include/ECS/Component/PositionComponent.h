c01f90ced68ea0b9831a6f28648ca0e645ef131d
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-26 09:21:44 +0100

    Reorganized PositionComponent. speed member now set in ctor initializer.

--- include/ECS/Component/PositionComponent.h
+++ include/ECS/Component/PositionComponent.h
@@ -9,17 +9,20 @@ namespace ECS::Component
     {
     public:
             PositionComponent(double x, double y, double vx, double vy)
-                : Component("Position"), x(x), y(y), vx(vx), vy(vy) {}
+                : Component("Position"), x(x),
+                  y(y),
+                  vx(vx),
+                  vy(vy),
+                  speed(GetRandomValue(-50,50))
+            {}
 
             ~PositionComponent() {}
-
-            virtual double getX() { return this->x;}
 
             double x;
             double y;
             double vx;
             double vy;
-            double speed = GetRandomValue(-5,5);
+            double speed;
     };
 }
 
