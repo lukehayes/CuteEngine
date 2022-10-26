77558077b0a9ce3aafd4afec8b45982245e0fcb7
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-26 09:22:52 +0100

    Implemented requirements based on which entities have the specific components.

--- include/ECS/System/PositionSystem.h
+++ include/ECS/System/PositionSystem.h
@@ -19,9 +19,46 @@ namespace ECS::System
             {
                 std::vector<ECS::Entity::Entity> ents;
                 int requirement_size = this->requirements.size();
+                int requirement_count = 0;
+
+                // TODO Brute force method of checking if an entity
+                // has the required components - refactor.
+                for(auto& entity : world.entities)
+                {
+                    for(auto& req : this->requirements)
+                    {
+                        if(entity.hasComponent(req))
+                        {
+                            requirement_count++;
+                        }
+                    }
+
+                    if(requirement_count == requirement_size)
+                    {
+                        auto pos = std::dynamic_pointer_cast<ECS::Component::PositionComponent>(entity.getComponent("Position"));
+
+                        pos->x += pos->speed * pos->vx * delta;
+                        pos->y += pos->speed * pos->vy * delta;
+
+                        if(pos->x < 0 || pos->x > 580)
+                        {
+                            pos->vx = -pos->vx;
+                        }
+
+                        if(pos->y < 0 || pos->y > 480)
+                        {
+                            pos->vy = -pos->vy;
+                        }
+
+                        requirement_count = 0;
+                    }else
+                    {
+                        requirement_count = 0;
+                    }
+                }
+
 
                 // TODO Implement entity selection based on requirements.
-
 
                 // for(auto& component : this->components)
                 // {
