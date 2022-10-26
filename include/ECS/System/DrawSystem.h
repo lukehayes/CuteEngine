3b3e482d49e449ec353b799863e4d18f43db3166
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-26 09:48:41 +0100

    Added ECS::System::DrawSystem to project.

--- include/ECS/System/DrawSystem.h
+++ include/ECS/System/DrawSystem.h
@@ -12,15 +12,41 @@ namespace ECS::System
             DrawSystem() : System("Draw") {}
             ~DrawSystem() {}
 
-            void update(float delta)
-            {
-            }
-
             void render()
             {
+                int requirement_size = this->requirements.size();
+                int requirement_count = 0;
 
+                ClearBackground(GRAY);
+                BeginDrawing();
+
+                // TODO Brute force method of checking if an entity
+                // has the required components - refactor.
+                for(auto& entity : world.entities)
+                {
+                    for(auto& req : this->requirements)
+                    {
+                        if(entity.hasComponent(req))
+                        {
+                            requirement_count++;
+                        }
+                    }
+
+                    if(requirement_count == requirement_size)
+                    {
+                        auto pos = std::dynamic_pointer_cast<ECS::Component::PositionComponent>(entity.getComponent("Position"));
+                        auto color = std::dynamic_pointer_cast<ECS::Component::ColorComponent>(entity.getComponent("Color"));
+                        DrawRectangle(pos->x, pos->y, 30,30, {color->r, color->g, color->b, color->a});
+
+                        requirement_count = 0;
+                    }else
+                    {
+                        requirement_count = 0;
+                    }
+                }
+
+                EndDrawing();
             }
-        std::vector< std::shared_ptr<ECS::Component::DrawComponent> > components;
     };
 }
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "ECS/Entity/Entity.h"
 #include "ECS/Component/Component.h"
 #include "ECS/Component/PositionComponent.h"
+#include "ECS/Component/ColorComponent.h"
 #include "ECS/System/PositionSystem.h"
+#include "ECS/System/DrawSystem.h"
 #include "ECS/System/System.h"
 #include "ECS/World.h"
 #include <memory>
@@ -13,7 +15,10 @@ int main() {
   game.init();
 
   ECS::System::PositionSystem positionSystem;
-  positionSystem.required({"Position"});
+  positionSystem.required({"Position", "Color"});
+
+  ECS::System::DrawSystem drawSystem;
+  drawSystem.required({"Position", "Color"});
 
   // FIXME Work out and refactor this.
   //
@@ -22,9 +27,12 @@ int main() {
   ECS::Entity::Entity ee(2);
   std::shared_ptr<ECS::Component::Component> posPtr = std::make_shared<ECS::Component::PositionComponent>(100,100,10,10);
   std::shared_ptr<ECS::Component::Component> posPtr2 = std::make_shared<ECS::Component::PositionComponent>(200,300,10,10);
+  std::shared_ptr<ECS::Component::Component> color  = std::make_shared<ECS::Component::ColorComponent>(50,100,100,255);
 
   e.addComponent("Position", posPtr);
+  e.addComponent("Color", color);
   ee.addComponent("Position", posPtr2);
+  ee.addComponent("Color", color);
 
   world.entities.push_back(e);
   world.entities.push_back(ee);
@@ -39,19 +47,7 @@ int main() {
     world.update(delta);
 
     positionSystem.update(delta);
-
-    ClearBackground(GRAY);
-        std::shared_ptr<ECS::Component::PositionComponent> pos = std::dynamic_pointer_cast<ECS::Component::PositionComponent>(e.getComponent("Position"));
-
-        for(auto e : world.entities)
-        {
-            std::shared_ptr<ECS::Component::PositionComponent> pos = std::dynamic_pointer_cast<ECS::Component::PositionComponent>(e.getComponent("Position"));
-            DrawRectangle(pos->x, pos->y, 30,30, GREEN);
-        }
-
-    BeginDrawing();
-        
-    EndDrawing();
+    drawSystem.render();
   }
 
   CloseWindow();
