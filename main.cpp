ac2ee26c2fb9e5f87d7977e046265513749a66e3
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-26 09:25:47 +0100

    All entities are now drawn in main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
   game.init();
 
   ECS::System::PositionSystem positionSystem;
-  positionSystem.required({"Position", "Sprite"});
+  positionSystem.required({"Position"});
 
   // FIXME Work out and refactor this.
   //
@@ -21,13 +21,18 @@ int main() {
   ECS::Entity::Entity e(1);
   ECS::Entity::Entity ee(2);
   std::shared_ptr<ECS::Component::Component> posPtr = std::make_shared<ECS::Component::PositionComponent>(100,100,10,10);
+  std::shared_ptr<ECS::Component::Component> posPtr2 = std::make_shared<ECS::Component::PositionComponent>(200,300,10,10);
+
   e.addComponent("Position", posPtr);
+  ee.addComponent("Position", posPtr2);
 
   world.entities.push_back(e);
+  world.entities.push_back(ee);
 
   // std::shared_ptr<ECS::Component::PositionComponent> pos = e.getComponent("Position");
   // std::shared_ptr<ECS::Component::PositionComponent> pos = std::dynamic_pointer_cast<ECS::Component::PositionComponent>(e.getComponent("Position"));
   /* ================================================================== */
+    
 
   while (!WindowShouldClose()) {
     delta = GetFrameTime();
@@ -35,10 +40,17 @@ int main() {
 
     positionSystem.update(delta);
 
-
     ClearBackground(GRAY);
+        std::shared_ptr<ECS::Component::PositionComponent> pos = std::dynamic_pointer_cast<ECS::Component::PositionComponent>(e.getComponent("Position"));
+
+        for(auto e : world.entities)
+        {
+            std::shared_ptr<ECS::Component::PositionComponent> pos = std::dynamic_pointer_cast<ECS::Component::PositionComponent>(e.getComponent("Position"));
+            DrawRectangle(pos->x, pos->y, 30,30, GREEN);
+        }
+
     BeginDrawing();
-        DrawRectangle(10, 10, 10, 10, BLACK);
+        
     EndDrawing();
   }
 
