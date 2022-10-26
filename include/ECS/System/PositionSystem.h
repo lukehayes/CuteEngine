3e0c7d78b0f194324b6a9a0c0b3d0b6dc7c78917
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-26 09:49:08 +0100

    Removed old movement code from ECS::System::PositionSystem

--- include/ECS/System/PositionSystem.h
+++ include/ECS/System/PositionSystem.h
@@ -17,7 +17,6 @@ namespace ECS::System
 
             void update(float delta)
             {
-                std::vector<ECS::Entity::Entity> ents;
                 int requirement_size = this->requirements.size();
                 int requirement_count = 0;
 
@@ -56,29 +55,7 @@ namespace ECS::System
                         requirement_count = 0;
                     }
                 }
-
-
-                // TODO Implement entity selection based on requirements.
-
-                // for(auto& component : this->components)
-                // {
-                //     component->x += component->speed * component->vx * delta;
-                //     component->y += component->speed * component->vy * delta;
-                //
-                //     if(component->x < 0 || component->x >= game.width)
-                //     {
-                //         component->vx = -component->vx;
-                //     }
-                //
-                //     if(component->y < 0 || component->y >= game.width)
-                //     {
-                //         component->vy = -component->vy;
-                //     }
-                //
-                // }
             }
-
-        std::vector< std::shared_ptr<ECS::Component::PositionComponent> > components;
     };
 }
 
