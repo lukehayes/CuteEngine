a7d950594e8e7d0521686dc0d629142df5a479ed
Luke Hayes <lukepurplehayes@gmail.com>
2023-04-11 10:46:47 +0100

    Added basic move system in main for testing.

--- main.cpp
+++ main.cpp
@@ -9,26 +9,39 @@
 
 float delta = 0.0;
 
-constexpr int MAX_ENTITIES = 3;
+// Stack limit is 1000000;
+constexpr int MAX_ENTITIES = 100;
+constexpr int SPEED = 200;
 
 int main() {
 
     game.init();
 
+    printf("Initializing %i Entities \n", MAX_ENTITIES);
+
+    // Initialize entities
     std::array<std::array<ECS::Component*, 3>, MAX_ENTITIES> entities;
 
     for(int i = 0; i <= MAX_ENTITIES - 1; i++)
     {
-        // std::vector<ECS::Component*> components = entities.at(i);
-        // components.at(0) = new ECS::TransformComponent(10,10, GetRandomValue(10,400), GetRandomValue(10,400));
-        entities[i][0] = new ECS::TransformComponent(GetRandomValue(10,300), GetRandomValue(10,300), 10,10, PINK);
+
+        Color color = {
+            (unsigned char)GetRandomValue(10,255),
+            (unsigned char)GetRandomValue(10,255),
+            (unsigned char)GetRandomValue(10,255),
+            (unsigned char)GetRandomValue(200,255)
+        };
+
+        entities[i][0] = new ECS::TransformComponent(GetRandomValue(10,game.width), GetRandomValue(10,game.height), 10,10, color);
         entities[i][1] = nullptr;
         entities[i][2] = nullptr;
     }
+    // --------------------------------------------------- 
 
 
     while (!WindowShouldClose()) {
         delta = GetFrameTime();
+        
 
         ClearBackground(BLACK);
         BeginDrawing();
@@ -45,6 +58,23 @@ int main() {
                 tc->size.y,
                 tc->color
             );
+
+            tc->position.x += tc->dx * SPEED * delta;
+            tc->position.y += tc->dy * SPEED * delta;
+
+
+            
+            if(
+                tc->position.x < 10 || tc->position.x > (game.width - 20) ||
+                tc->position.y < 10 || tc->position.y > (game.height - 20)
+            )
+            {
+                tc->position.x = -tc->dx;
+                tc->position.y = -tc->dy;
+
+            }
+
+
         }
 
 
