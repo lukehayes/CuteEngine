42fc4e824dc0137b5fd753cf739e2e434957c2d1
lukehayes <lukepurplehayes@gmail.com>
2023-04-11 17:50:09 +0100

    Main is now using ECS::RenderSystem and the EntityArray type.

--- main.cpp
+++ main.cpp
@@ -3,15 +3,22 @@
 #include "ECS/Component.h"
 #include "ECS/TransformComponent.h"
 
+#include "ECS/System/RenderSystem.h"
+
+#include "common/types.h"
+
 #include <cstdio>
 #include <vector>
 #include <array>
 
+/*============================================================
+// Useful Variables
+============================================================*/
 float delta = 0.0;
 
 // Stack limit is 1000000;
-constexpr int MAX_ENTITIES = 100;
-constexpr int SPEED = 200;
+// constexpr int MAX_ENTITIES = 100;
+
 
 int main() {
 
@@ -20,8 +27,6 @@ int main() {
     printf("Initializing %i Entities \n", MAX_ENTITIES);
 
     // Initialize entities
-    std::array<std::array<ECS::Component*, 3>, MAX_ENTITIES> entities;
-
     for(int i = 0; i <= MAX_ENTITIES - 1; i++)
     {
         Color color = {
@@ -31,40 +36,31 @@ int main() {
             (unsigned char)GetRandomValue(200,255)
         };
 
-        int size = GetRandomValue(2,20);
+        int size = 1;
 
-        entities[i][0] = new ECS::TransformComponent(GetRandomValue(10,game.width), GetRandomValue(10,game.height), size, size, color);
+        entities[i][0] = new ECS::TransformComponent(GetRandomValue(100,game.width), GetRandomValue(100,game.height), size, size, color);
         entities[i][1] = nullptr;
         entities[i][2] = nullptr;
     }
     // --------------------------------------------------- 
 
+
+    ECS::RenderSystem renderSystem;
+
+
     while (!WindowShouldClose()) {
         delta = GetFrameTime();
         
+        for(auto e : entities)
+        {
+            ECS::TransformComponent* tc = dynamic_cast<ECS::TransformComponent*>(e[0]);
+            tc->update(delta);
+        }
 
         ClearBackground(BLACK);
         BeginDrawing();
 
-
-        for(auto e : entities)
-        {
-            ECS::TransformComponent* tc = dynamic_cast<ECS::TransformComponent*>(e[0]);
-
-            tc->update(delta);
-
-            DrawRectangle(
-                tc->position.x,
-                tc->position.y,
-                tc->size.x,
-                tc->size.y,
-                tc->color
-            );
-
-        }
-
-
-        
+            renderSystem.update(delta);
 
         EndDrawing();
     }
