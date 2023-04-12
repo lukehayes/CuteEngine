c5dbb624f84a2297cd6ccd2afe452e76a73d084b
Luke Hayes <lukepurplehayes@gmail.com>
2023-04-12 09:34:50 +0100

    Corrected indentation in RenderSystem.cpp

--- src/RenderSystem.cpp
+++ src/RenderSystem.cpp
@@ -3,7 +3,6 @@
 
 #include "Game.h"
 
-
 namespace ECS
 {
   void
@@ -14,21 +13,20 @@ namespace ECS
 
     for(auto entity : entities)
     {
-      // Transform Component is at index 0.
-      if(!entity[0]) return;
+        // Transform Component is at index 0.
+        if(!entity[0]) return;
 
-      auto* tc = dynamic_cast<ECS::TransformComponent*>(entity[0]);
+        auto* tc = dynamic_cast<ECS::TransformComponent*>(entity[0]);
 
-      DrawRectangle(
-          tc->position.x,
-          tc->position.y,
-          tc->size.x,
-          tc->size.y,
-          tc->color
-      );
+        DrawRectangle(
+            tc->position.x,
+            tc->position.y,
+            tc->size.x,
+            tc->size.y,
+            tc->color
+        );
     }
 
     EndDrawing();
-
   }
 }
