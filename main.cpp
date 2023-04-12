57f34cafa1a0ead8918154c8f4249e62874393d1
Luke Hayes <lukepurplehayes@gmail.com>
2023-04-12 09:35:01 +0100

    Added Entity array layout comment

--- main.cpp
+++ main.cpp
@@ -26,6 +26,15 @@ int main() {
     /*============================================================
     // Entity Initializtion
     ============================================================*/
+
+    /**
+     * Entity Array Layout:
+     *
+     * [0] Transform Component - Always defined
+     * [1] Sprite Component    - Can be null
+     * [2] Timer Component     - Can be null - Possible Map
+     * [3] Sound Component     - Can be null - Possible Map
+     */
     for(int i = 0; i <= MAX_ENTITIES - 1; i++)
     {
         Color color = {
@@ -42,13 +51,11 @@ int main() {
         entities[i][2] = nullptr;
     }
 
-
     /*============================================================
     // System Initializtion
     ============================================================*/
     ECS::RenderSystem renderSystem;
     ECS::MoveSystem   moveSystem;
-
 
     while (!WindowShouldClose()) {
         game.deltaTime = GetFrameTime();
