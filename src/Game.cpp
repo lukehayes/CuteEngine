80c2213fd68b031e49fac8e80cf41e1dc4393334
lukehayes <lukepurplehayes@gmail.com>
2023-04-13 17:21:59 +0100

    Corrected indentation of Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,26 +1,22 @@
 #include "Game.h"
 #include "common/Constants.h"
 
-	Game::Game()
-		: width(WINDOW_WIDTH),
-		  height(WINDOW_HEIGHT),
-		  title(GAME_TITLE)
-	{}
+Game::Game()
+	: width(WINDOW_WIDTH),
+	  height(WINDOW_HEIGHT),
+	  title(GAME_TITLE)
+{}
 
 void Game::init()
 {
 	InitWindow(
-			this->width,
-			this->height,
-			this->title
-		  );
+		this->width,
+		this->height,
+		this->title
+	);
 
 	InitAudioDevice();
 
 	SetTargetFPS(60);
 	SetExitKey(KEY_Q);
 }
-
-Game game = {};
-
-EntityArray entities = {};
