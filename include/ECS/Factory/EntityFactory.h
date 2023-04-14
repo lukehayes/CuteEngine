6d4e40375ca26c9dcb46e4124acdee04ac31d4ef
Luke Hayes <lukepurplehayes@gmail.com>
2023-04-14 10:13:21 +0100

    Added ECS::EntityFactory to project.

---
+++ include/ECS/Factory/EntityFactory.h
@@ -0,0 +1,143 @@
+#ifndef ECS_ENTITY_FACTORY_H
+#define ECS_ENTITY_FACTORY_H
+
+#include <raylib.h>
+#include <stdio.h>
+
+#include "ECS/Component/Component.h"
+#include "ECS/Component/TransformComponent.h"
+#include "ECS/Component/SoundComponent.h"
+
+#include "Common/Constants.h"
+#include "Common/Globals.h"
+
+namespace ECS
+{
+    EntityVector GenerateEntities(int count)
+    {
+        printf("Initializing %i Entities \n", count);
+
+        EntityVector entities;
+
+        /*============================================================
+        // Entity Initializtion
+        ============================================================*/
+        /**
+         * Entity Array Layout:
+         *
+         * [0] Transform Component - Always defined
+         * [1] Sprite Component    - Can be null
+         * [2] Timer Component     - Can be null - Possible Map
+         * [3] Sound Component     - Can be null - Possible Map
+         */
+        for(int i = 0; i <= count - 1; i++)
+        {
+            Color color = {
+                (unsigned char)GetRandomValue(10,255),
+                (unsigned char)GetRandomValue(10,255),
+                (unsigned char)GetRandomValue(10,255),
+                (unsigned char)GetRandomValue(200,255)
+            };
+
+            int size = 10;
+
+            entities[i][TRANSFORM_COMPONENT_INDEX] = new ECS::TransformComponent(GetRandomValue(100,game.width), GetRandomValue(100,game.height), size, size, color);
+            entities[i][1] = nullptr;
+            entities[i][2] = nullptr;
+            entities[i][SOUND_COMPONENT_INDEX] = new ECS::SoundComponent("../assets/sounds/blip.wav");
+        }
+
+        return entities;
+    }
+
+    /**
+     * Destroy all the entities using the EXTERN variable 
+     * defined in common/Globals.h
+     */
+    void DestroyEntities(EntityVector& entities, int count)
+    {
+        // TODO Cheeky memory allocation cleanup.
+        for(int i = 0; i <= count - 1; i++)
+        {
+            auto* tc = dynamic_cast<ECS::TransformComponent*>(entities[i][TRANSFORM_COMPONENT_INDEX]);
+            auto* sc = dynamic_cast<ECS::SoundComponent*>(entities[i][SOUND_COMPONENT_INDEX]);
+
+            delete tc;
+            tc = nullptr;
+            printf("Deleted Transform Component %i\n", i);
+
+            delete sc;
+            sc = nullptr;
+            printf("Deleted Sound Component %i\n", i);
+        }
+
+        printf("All memory deleted \n");
+    }
+    /*============================================================
+    // Extern Versions
+    ============================================================*/
+    /**
+     * Generate all the entities using the EXTERN variable 
+     * defined in common/Globals.h
+     */
+    void GenerateEntitiesExtern(int count)
+    {
+        printf("Initializing %i Entities \n", count);
+
+        /*============================================================
+        // Entity Initializtion
+        ============================================================*/
+        /**
+         * Entity Array Layout:
+         *
+         * [0] Transform Component - Always defined
+         * [1] Sprite Component    - Can be null
+         * [2] Timer Component     - Can be null - Possible Map
+         * [3] Sound Component     - Can be null - Possible Map
+         */
+        for(int i = 0; i <= count - 1; i++)
+        {
+            Color color = {
+                (unsigned char)GetRandomValue(10,255),
+                (unsigned char)GetRandomValue(10,255),
+                (unsigned char)GetRandomValue(10,255),
+                (unsigned char)GetRandomValue(200,255)
+            };
+
+            int size = 10;
+
+            entities[i][TRANSFORM_COMPONENT_INDEX] = new ECS::TransformComponent(GetRandomValue(100,game.width), GetRandomValue(100,game.height), size, size, color);
+            entities[i][1] = nullptr;
+            entities[i][2] = nullptr;
+            entities[i][SOUND_COMPONENT_INDEX] = new ECS::SoundComponent("../assets/sounds/blip.wav");
+        }
+    }
+
+
+    /**
+     * Destroy all the entities using the EXTERN variable 
+     * defined in common/Globals.h
+     */
+    void DestroyEntitiesExtern(int count)
+    {
+        // TODO Cheeky memory allocation cleanup.
+        for(int i = 0; i <= count - 1; i++)
+        {
+            auto* tc = dynamic_cast<ECS::TransformComponent*>(entities[i][TRANSFORM_COMPONENT_INDEX]);
+            auto* sc = dynamic_cast<ECS::SoundComponent*>(entities[i][SOUND_COMPONENT_INDEX]);
+
+            delete tc;
+            tc = nullptr;
+            printf("Deleted Transform Component %i\n", i);
+
+            delete sc;
+            sc = nullptr;
+            printf("Deleted Sound Component %i\n", i);
+        }
+
+        printf("All memory deleted \n");
+    }
+
+}
+
+#endif // ECS_ENTITY_FACTORY_H
--- main.cpp
+++ main.cpp
@@ -2,12 +2,9 @@
 #include "Common/Constants.h"
 #include "Common/Globals.h"
 
-#include "ECS/Component/Component.h"
-#include "ECS/Component/TransformComponent.h"
-#include "ECS/Component/SoundComponent.h"
-
 #include "ECS/System/RenderSystem.h"
 #include "ECS/System/MoveSystem.h"
+#include "ECS/Factory/EntityFactory.h"
 #include <stdio.h>
 
 int main() {
@@ -15,41 +12,14 @@ int main() {
     game.init();
     game.deltaTime = 0.0;
 
-    printf("Initializing %i Entities \n", MAX_ENTITIES);
-
-    /*============================================================
-    // Entity Initializtion
-    ============================================================*/
-    /**
-     * Entity Array Layout:
-     *
-     * [0] Transform Component - Always defined
-     * [1] Sprite Component    - Can be null
-     * [2] Timer Component     - Can be null - Possible Map
-     * [3] Sound Component     - Can be null - Possible Map
-     */
-    for(int i = 0; i <= MAX_ENTITIES - 1; i++)
-    {
-        Color color = {
-            (unsigned char)GetRandomValue(10,255),
-            (unsigned char)GetRandomValue(10,255),
-            (unsigned char)GetRandomValue(10,255),
-            (unsigned char)GetRandomValue(200,255)
-        };
-
-        int size = 10;
-
-        entities[i][TRANSFORM_COMPONENT_INDEX] = new ECS::TransformComponent(GetRandomValue(100,game.width), GetRandomValue(100,game.height), size, size, color);
-        entities[i][1] = nullptr;
-        entities[i][2] = nullptr;
-        entities[i][SOUND_COMPONENT_INDEX] = new ECS::SoundComponent("../assets/sounds/blip.wav");
-    }
+    EntityVector ev = ECS::GenerateEntities(MAX_ENTITIES);
 
     /*============================================================
     // System Initializtion
     ============================================================*/
     ECS::RenderSystem renderSystem;
     ECS::MoveSystem   moveSystem;
+
 
     while (!WindowShouldClose()) {
         game.deltaTime = GetFrameTime();
@@ -58,23 +28,7 @@ int main() {
         renderSystem.update(BLACK, game.deltaTime);
     }
 
-    // TODO Cheeky memory allocation cleanup.
-    for(int i = 0; i <= MAX_ENTITIES - 1; i++)
-    {
-        auto* tc = dynamic_cast<ECS::TransformComponent*>(entities[i][TRANSFORM_COMPONENT_INDEX]);
-        auto* sc = dynamic_cast<ECS::SoundComponent*>(entities[i][SOUND_COMPONENT_INDEX]);
-
-        delete tc;
-        tc = nullptr;
-        printf("Deleted Transform Component %i\n", i);
-
-        delete sc;
-        sc = nullptr;
-        printf("Deleted Sound Component %i\n", i);
-
-    }
-
-    printf("All memory deleted \n");
+    ECS::DestroyEntities(ev, MAX_ENTITIES);
 
     CloseWindow();
 
