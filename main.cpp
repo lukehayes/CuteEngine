2c35ad2a99dee2c777083a89d5cb7c29b3d6ad05
Luke Hayes <lukepurplehayes@gmail.com>
2023-04-14 09:32:33 +0100

    Entity components are now deleted at the end of the program.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 #include "ECS/System/RenderSystem.h"
 #include "ECS/System/MoveSystem.h"
+#include <stdio.h>
 
 int main() {
 
@@ -56,6 +57,24 @@ int main() {
         moveSystem.update(game.deltaTime);
         renderSystem.update(BLACK, game.deltaTime);
     }
+
+    // TODO Cheeky memory allocation cleanup.
+    for(int i = 0; i <= MAX_ENTITIES - 1; i++)
+    {
+        auto* tc = dynamic_cast<ECS::TransformComponent*>(entities[i][TRANSFORM_COMPONENT_INDEX]);
+        auto* sc = dynamic_cast<ECS::SoundComponent*>(entities[i][SOUND_COMPONENT_INDEX]);
+
+        delete tc;
+        tc = nullptr;
+        printf("Deleted Transform Component %i\n", i);
+
+        delete sc;
+        sc = nullptr;
+        printf("Deleted Sound Component %i\n", i);
+
+    }
+
+    printf("All memory deleted \n");
 
     CloseWindow();
 
