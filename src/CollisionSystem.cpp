178cb1f349ca8c7dd0ae2cb0745b700798e07200
lukehayes <lukepurplehayes@gmail.com>
2023-04-17 16:18:09 +0100

    CollisionSystem now iterates through every other entity.

--- src/CollisionSystem.cpp
+++ src/CollisionSystem.cpp
@@ -17,20 +17,25 @@ namespace ECS
 
         for(int i = 0; i <= MAX_ENTITIES - 2; i++)
         {
-            auto tc = dynamic_cast<ECS::TransformComponent*>(entities[i][TRANSFORM_COMPONENT_INDEX]);
-            auto tn = dynamic_cast<ECS::TransformComponent*>(entities[i + 1][TRANSFORM_COMPONENT_INDEX]);
 
-            if(
-                tc->position.x > tn->position.x &&
-                tc->position.x < tn->size.x &&
-                tc->position.y > tn->position.y &&
-                tc->position.y < tn->size.y
-            ){
-                int size = GetRandomValue(3,100);
-                tc->size.x = size;
-                tc->size.y = size;
-                printf("HIT \n");
-            }else {
+            auto* tc = dynamic_cast<ECS::TransformComponent*>(entities[i][TRANSFORM_COMPONENT_INDEX]);
+
+            for(int j = 0; j <= MAX_ENTITIES - 1; j++)
+            {
+                auto* tn = dynamic_cast<ECS::TransformComponent*>(entities[j][TRANSFORM_COMPONENT_INDEX]);
+
+                if( tc->position.x > tn->position.x &&
+                    tc->position.x < tn->size.x &&
+                    tc->position.y > tn->position.y &&
+                    tc->position.y < tn->size.y ){
+
+                        int size = GetRandomValue(3,20);
+                        tc->size.x = size;
+                        tc->size.y = size;
+                        printf("HIT \n");
+                }
+
+
             }
         }
 
