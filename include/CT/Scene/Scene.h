d35e4e1d6855d80508a587baca025625e287cc03
Luke Hayes <lukepurplehayes@gmail.com>
2023-08-02 09:57:44 +0100

    Added start of SceneTree implementation.

--- include/CT/Scene/Scene.h
+++ include/CT/Scene/Scene.h
@@ -1,6 +1,8 @@
 #ifndef CT_SCENE_SCENE_H
 #define CT_SCENE_SCENE_H
 
+#include <map>
+
 namespace CT::Scene
 {
 
@@ -13,6 +15,14 @@ namespace CT::Scene
 
     virtual void update(double dt) = 0;
 
+    void addChild(CT::Scene::Scene* child)
+    {
+      this->children.insert(std::pair(childrenCount, child));
+      childrenCount++;
+    }
+
+    std::map<int, CT::Scene::Scene*> children;
+    inline static int childrenCount = 0;
   };
 }
 #endif // CT_SCENE_SCENE_H
