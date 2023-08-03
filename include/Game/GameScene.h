cd9f325007315968e4d247c681a0dad23299fcb7
Luke Hayes <lukepurplehayes@gmail.com>
2023-08-03 10:11:19 +0100

    Added Game::GameScene test class to project.

---
+++ include/Game/GameScene.h
@@ -0,0 +1,25 @@
+#ifndef GAME_GAMESCENE_H
+#define GAME_GAMESCENE_H
+
+#include "CT/Scene/Scene.h"
+
+namespace Game
+{
+class GameScene : public CT::Scene::Scene
+{
+public:
+	GameScene();
+
+	//GameScene(GameScene &&) = default;
+	//GameScene(const GameScene &) = default;
+	//GameScene &operator=(GameScene &&) = default;
+	//GameScene &operator=(const GameScene &) = default;
+	~GameScene();
+
+	void update(double dt);
+
+private:
+};
+}
+
+#endif // !GAME_GAMESCENE_H
---
+++ src/Game/GameScene.cpp
@@ -0,0 +1,22 @@
+#include "Game/GameScene.h"
+
+#include "stdio.h"
+
+namespace Game
+{
+	GameScene::GameScene()
+	{
+	}
+
+	GameScene::~GameScene()
+	{
+	}
+
+	void GameScene::update(double dt)
+	{
+		printf("Updating Game Scene: %f \n", dt);
+	}
+}
+
+
+
