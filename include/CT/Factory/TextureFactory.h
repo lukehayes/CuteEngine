62bddfb5f5964ee1e3f17b939bfe2aba0e62c151
lukehayes <lukepurplehayes@gmail.com>
2023-08-08 17:55:57 +0100

    Moved TextureFactory implementation to .cpp file.

--- include/CT/Factory/TextureFactory.h
+++ include/CT/Factory/TextureFactory.h
@@ -10,45 +10,12 @@ namespace CT::Factory
     {
     public:
 
-        TextureFactory()
-        {
-            this->loadAssetsIntoGPUMemory();
-        }
+        TextureFactory();
+        ~TextureFactory();
+        void loadTexturePointers();
+        void destroyTexturePointers();
 
-        void loadToTexturePtr(Texture2D* texture, const std::string& imagePath)
-        {
-            std::string assets = CT::Asset::Asset::getAssetPath();
-            printf("Loading texture to Texture2D*");
-            *texture = LoadTexture((assets + imagePath).c_str());
-        }
-
-        void unloadFromTexturePtr(Texture2D* texture)
-        {
-            printf("Unloading Texture Pointer \n");
-            UnloadTexture(*texture);
-        }
-        
-        /**
-         * Load all of the default textures into the GPU.
-         */
-        void loadAssetsIntoGPUMemory()
-        {
-            std::string assets = CT::Asset::Asset::getAssetPath();
-            printf("Assets: %s \n", assets.c_str());
-            this->bernie = LoadTexture((assets + "/bernie.png").c_str());
-        }
-
-        /**
-         * Load all of the default textures into the GPU.
-         */
-        void unloadAssetsIntoGPUMemory()
-        {
-            UnloadTexture(this->bernie);
-            printf("Unloaded bernie.png texture \n");
-        }
-
-        Texture2D bernie;
-        Texture2D* testTexture;
+        Texture2D textures[10];
     };
 }
 
---
+++ src/CT/TextureFactory.cpp
@@ -0,0 +1,35 @@
+#include "CT/Factory/TextureFactory.h"
+#include <iostream>
+#include <raylib.h>
+
+namespace CT::Factory
+{
+    TextureFactory::TextureFactory()
+    {
+        this->loadTexturePointers();
+    }
+
+    TextureFactory::~TextureFactory()
+    {
+    }
+    void 
+    TextureFactory::destroyTexturePointers()
+    {
+        for(int i = 0; i <= 10 - 1; i++)
+        {
+            UnloadTexture(this->textures[i]);
+        }
+        
+        TraceLog(LOG_DEBUG, "Textures unloaded from texture factory.") ;
+    }
+
+    void 
+    TextureFactory::loadTexturePointers()
+    {
+        std::string assets = CT::Asset::Asset::getAssetPath();
+        Texture2D spritesheet = LoadTexture(("../assets/images/monochrome_tilemap_transparent.png"));
+        this->textures[0] = spritesheet;
+    }
+        
+}
+
