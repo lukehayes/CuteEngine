83601817e965987debbf6c8854a90d1b125f08cb
lukehayes <lukepurplehayes@gmail.com>
2024-05-01 17:56:20 +0100

    Removed TextureFactory ctors.

--- src/CT/TextureFactory.cpp
+++ src/CT/TextureFactory.cpp
@@ -1,32 +1,9 @@
 #include "CT/Factory/TextureFactory.h"
 #include "CT/Common/Globals.h"
 
-
 namespace CT::Factory 
 {
-
-TextureFactory::TextureFactory(const std::string& imagePath)
-{
-    std::string imageAsset = CT::Asset::Asset::getImage(imagePath);
-    this->texture = LoadTexture(imageAsset.c_str());
-}
-
-TextureFactory::~TextureFactory()
-{
-    UnloadTexture(this->texture);
-}
-
-
-std::shared_ptr<Texture2D>
-TextureFactory::loadToTexturePtr(const std::string& imagePath)
-{
-    std::string imageAsset = CT::Asset::Asset::getImage(imagePath);
-    const char* img_asset_path = imageAsset.c_str();
-
-    std::shared_ptr<Texture2D> texture_ptr = std::make_shared<Texture2D>(LoadTexture(img_asset_path));
-
-    return texture_ptr;
-}
+// XXX Not Implemented.
 }
 
 
